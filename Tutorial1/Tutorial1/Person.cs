054d4cc6a687217c19f65e46b7daf55fa653dfd1
s16500 <s16500@LAB132-11>
2020-03-06 15:35:23 +0100

    tutorial1

---
+++ Tutorial1/Tutorial1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial1
+{
+    class Person
+    {
+        private String firstname;
+
+        public string Firstname
+        {
+
+            get { return Firstname; }
+            set {
+                Firstname = value;
+                    }
+            //public string Firstname { get; set; }
+
+        }
+
+    }
+}
---
+++ Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            //int? a = null;
+            // Boolean b = false;
+            //int age = 25;
+            // string m = "mustfa";
+
+
+
+
+            //Console.WriteLine($"i am  { age} years old);
+            //Console.WriteLine($"i am  { age} years old");
+            //Console.WriteLine("Hello World!");
+
+            var url = @"https://www.pja.edu.pl/";
+
+
+            var httpClient = new HttpClient();
+
+
+            var response =await httpClient.GetAsync(url);
+
+
+            var content = response.Content.ReadAsStringAsync();
+
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+",RegexOptions.IgnoreCase);
+
+
+            var matches = regex.Matches(content);
+
+        foreach (var match in matches)
+            {
+
+                Console.WriteLine(match.ToString());
+
+
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
